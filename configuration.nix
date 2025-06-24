a1cb00ac111612cb5e86672983fc49e80d2f8dfb
gawakawa <mashurakiryu@icloud.com>
2025-06-24 21:15:04 +0900

    ➕ Add noto fonts

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,30 @@ in
       fira-code
       fira-code-symbols
       nerd-fonts.fira-code
+
+      noto-fonts-cjk-serif
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      nerd-fonts.noto
     ];
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Noto Serif CJK JP"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [
+          "Noto Sans CJK JP"
+          "NOto Color Emoji"
+        ];
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "Noto Color Emoji"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
   };
 
   # Flakes用の設定
