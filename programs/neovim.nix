b572f773653d9202d29021b6fc721f3199cbc2dc
gawakawa <mashurakiryu@icloud.com>
2025-08-10 17:53:50 +0900

    ♻️ Unify platform-specific neovim configs

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -4,10 +4,6 @@
   lib,
   ...
 }:
-let
-  isLinux = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
-  isDarwin = pkgs.stdenv.hostPlatform.system == "aarch64-darwin";
-in
 {
   programs.neovim = {
     enable = true;
@@ -18,28 +14,9 @@ in
     plugins = with pkgs.vimPlugins; [
       lazy-nvim
     ];
-
   };
 
-  # Linux特有の設定ファイルコピー
-  home.file =
-    if isLinux then
-      {
-        ".config/nvim" = {
-          source = ../nvim;
-          recursive = true;
-        };
-
-        # Exclude lazy-lock.json from being managed
-        ".config/nvim/lazy-lock.json".enable = false;
-      }
-    else if isDarwin then
-      {
-        # Darwin特有のシンボリックリンク作成
-        ".config/nvim" = {
-          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix-config/nvim";
-        };
-      }
-    else
-      { };
+  home.file.".config/nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix-config/nvim";
+  };
 }
