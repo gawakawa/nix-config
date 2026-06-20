8f8771ff5cc1b354d6af1b048e69f4777fa2417e
gawakawa <mashurakiryu@icloud.com>
2026-06-20 21:22:16 +0900

    Revert ":wrench: Pin fcitx5 input-method group (keyboard-us + mozc) declaratively"
    This reverts commit 8270db4880018dbfc0689b15b44cb45306fc92f3.

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -111,19 +111,7 @@
     inputMethod = {
       type = "fcitx5";
       enable = true;
-      fcitx5 = {
-        addons = [ pkgs.fcitx5-mozc ];
-        settings.inputMethod = {
-          "Groups/0" = {
-            Name = "Default";
-            "Default Layout" = "us";
-            DefaultIM = "keyboard-us";
-          };
-          "Groups/0/Items/0".Name = "keyboard-us";
-          "Groups/0/Items/1".Name = "mozc";
-          GroupOrder."0" = "Default";
-        };
-      };
+      fcitx5.addons = [ pkgs.fcitx5-mozc ];
     };
   };
 
